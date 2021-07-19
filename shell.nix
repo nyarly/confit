8b6c38c59e1760e85a784eca8fb90d1160a71aa2
Judson Lester <nyarly@gmail.com>
2021-07-19 13:32:21 -0700

    GHA update

--- shell.nix
+++ shell.nix
@@ -4,12 +4,15 @@ in
 { pkgs ? pinned }:
 
 pkgs.mkShell {
+
   buildInputs = with pkgs; [
     cargo
     cargo-cross
     rustup
     yj
     jq
+  ] ++ lib.optionals stdenv.isDarwin [
+    libiconv
   ];
   # Enable printing backtraces for rust binaries
   RUST_BACKTRACE = 1;
